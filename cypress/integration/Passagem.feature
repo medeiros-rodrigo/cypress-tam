832074f1f5e8159191481d5c38428c1d0188df81
Rodrigo Medeiros <rodrigomedeiros7191@gmail.com>
2021-08-01 01:30:25 -0400

    add origem==destino

--- cypress/integration/Passagem.feature
+++ cypress/integration/Passagem.feature
@@ -1,8 +1,25 @@
 Feature: Pesquisar Passagem
 
-Como usuario, eu quero pesquisar uma passagem area no Latam
+Como usuario, eu quero pesquisar uma passagem aerea no Latam
 
 Scenario: Pesquisar passagem para 1 passageiro
-    Given eu escolho um destino e uma data
-    When eu pesquiso uma passagem
-    Then deve obter resultados para a busca
+    Given que eu acesso o latam
+    When eu escolho um destino "Congonhas" - "Galeao"
+    And uma data
+    And eu pesquiso uma passagem
+    Then deve obter os meus dados de pesquisa
+    And retornar para "1" passageiros
+
+Scenario: : Pesquisar passagem para 1 passageiro e 1 bebe
+    Given que eu acesso o latam
+    When eu escolho um destino "Congonhas" - "Galeao"
+    And uma data
+    And eu adiciono um passageiro ´Bebe´
+    And eu pesquiso uma passagem
+    Then deve obter os meus dados de pesquisa
+    And retornar para "2" passageiros
+
+Scenario: : Pesquisar passagem com a mesma origem e destino
+    Given que eu acesso o latam
+    When eu escolho um destino "Joao Pessoa" - "Joao Pessoa"
+    Then deve retornar alerta com erro
--- cypress/integration/Passagem/steps.js
+++ cypress/integration/Passagem/steps.js
@@ -1,27 +1,36 @@
-Given(/^eu escolho um destino e uma data$/, () => {
+
+Given(/^que eu acesso o latam$/, () => {
 	cy.visit('https://www.latamairlines.com/br/pt')
     cy.viewport(550, 750)
-
-	cy.xpath('/html/body/div[3]/div/div/div/div/button').click()
+    cy.xpath('/html/body/div[3]/div/div/div/div/button').click()
     cy.xpath('//*[@id="cookies-politics-button"]').click()
-        
+});
 
+
+
+When(/^eu escolho um destino "([^"]*)" - "([^"]*)"$/, (origem, destino) => {
     // Selecionar Ida
     cy.xpath('//*[@id="lblInputOrigin"]').click()
-    cy.xpath('//*[@id="txtInputOrigin"]').type('Congonhas')
+    cy.xpath('//*[@id="txtInputOrigin"]').type(origem)
     cy.xpath('//*[@id="btnItemAutoComplete_0"]').click()
     cy.xpath('//*[@id="btnContinueCTA"]').click()
         
     // selecionar Volta
     cy.xpath('//*[@id="lblInputDestination"]').click()
     cy.wait(1500)
-    cy.xpath('//*[@id="txtInputDestination"]').type('Galeao')
-    cy.wait(1500)
-    cy.xpath('//*[@id="btnItemAutoComplete_0"]').click()
-    cy.xpath('//*[@id="btnContinueCTA"]').click()
+    cy.xpath('//*[@id="txtInputDestination"]').type(destino)
 
-    cy.wait(1500)  
-    // Selecionar Data
+    if (origem != destino){
+        cy.wait(1500)
+        cy.xpath('//*[@id="btnItemAutoComplete_0"]').click()
+        cy.xpath('//*[@id="btnContinueCTA"]').click()
+
+        cy.wait(1500) 
+    }
+
+});
+
+When(/^uma data$/, () => {
     cy.xpath('//*[contains(text(),"Adicionar data")]').click()
     cy.xpath("(//*[@class='CalendarDay CalendarDay_1 CalendarDay__default CalendarDay__default_2 CalendarDay__lastDayOfWeek CalendarDay__lastDayOfWeek_3'])[3]").scrollIntoView()
     cy.xpath("(//*[@class='CalendarDay CalendarDay_1 CalendarDay__default CalendarDay__default_2 CalendarDay__lastDayOfWeek CalendarDay__lastDayOfWeek_3'])[3]").click({ force: true })
@@ -29,23 +38,33 @@ Given(/^eu escolho um destino e uma data$/, () => {
     cy.xpath("(//*[@class='CalendarDay CalendarDay_1 CalendarDay__default CalendarDay__default_2 CalendarDay__lastDayOfWeek CalendarDay__lastDayOfWeek_3'])[4]").click({ force: true })
 
     cy.xpath("//*[contains(text(),'Confirmar')]").click()
-
 });
 
 When(/^eu pesquiso uma passagem$/, () => {
 	cy.xpath("//*[contains(text(),'Procurar voos')]").click()
 });
 
-Then(/^deve obter resultados para a busca$/, () => {
-	// Validação: Número de Passageiros
-	cy.xpath("//*[@data-test='TestMobileTotalCantPax']").contains("1")
+Then(/^deve obter os meus dados de pesquisa$/, () => {
+		// Validação: Destino de Ida e Volta
+        cy.wait(10000)  
+        cy.xpath('//*[@id="WrapperOriginDestination"]/div[1]').contains("São Paulo").contains("Rio de Janeiro")
     
-	// Validação: Destino de Ida e Volta
-	cy.wait(10000)  
-	cy.xpath('//*[@id="WrapperOriginDestination"]/div[1]').contains("São Paulo").contains("Rio de Janeiro")
+        // Validação: Data de Viagem
+        cy.xpath("//*[@id='WrapperOriginDestination']/div[2]").contains("Dom 22 Ago")
+        cy.xpath("//*[@id='WrapperOriginDestination']/div[2]/span[3]").contains("Dom 5 Set")
+});
 
-	// Validação: Data de Viagem
-	cy.xpath("//*[@id='WrapperOriginDestination']/div[2]").contains("Dom 22 Ago")
-	cy.xpath("//*[@id='WrapperOriginDestination']/div[2]/span[3]").contains("Dom 5 Set")
-	
+Then(/^retornar para "([^"]*)" passageiros$/, (quantidadeDePassageiros) => {
+    cy.xpath("//*[@data-test='TestMobileTotalCantPax']").contains(quantidadeDePassageiros)
+});
+
+When(/^eu adiciono um passageiro ´Bebe´$/, () => {
+	cy.get('#iconAddPassengerCTA').click()
+    cy.get('#addInfant').click()
+    cy.get('#sendCallback').click()
+});
+
+
+Then(/^deve retornar alerta com erro$/, () => {
+	cy.get('.xp-Alert-Title').contains("Não encontramos um resultado que coincida com sua busca")
 });
